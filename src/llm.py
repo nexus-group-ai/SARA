0bbd4fcaecae6c0aa7fdfe6cd65c0f0453f02b4c
okcze <piotr.phitz@gmail.com>
2025-03-05 01:10:49 +0100

    Add RAG demo

--- src/llm.py
+++ src/llm.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 import os
 
 def get_llm_client(**kwargs):
@@ -6,4 +7,10 @@ def get_llm_client(**kwargs):
         model=os.getenv("OPENAI_MODEL"),
         api_key=os.getenv("OPENAI_API_KEY"),
         **kwargs
+    )
+
+def get_gemini_embeddings_client():
+    return GoogleGenerativeAIEmbeddings(
+        google_api_key=os.getenv("OPENAI_API_KEY"),
+        model="models/text-embedding-004",
     )
